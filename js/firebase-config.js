359e40afa339b252279e63d2d9389830711494d6
rzkypribadi <rzkyalamak@gmail.com>
2025-08-04 15:03:33 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyANSYmNZHRuFE6p6lga3cEkYTCQU67xT7g",
+  authDomain: "database-invest.firebaseapp.com",
+  databaseURL: "https://database-invest-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "database-invest",
+  storageBucket: "database-invest.firebasestorage.app",
+  messagingSenderId: "465429517765",
+  appId: "1:465429517765:web:de893fc118784569ef67ac",
+  measurementId: "G-XW756RZPZJ"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
+
+export { auth, db };
